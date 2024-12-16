be7b5cba381d4ecac22b6c8ce2a626282694e779
Dawid Szpunar <szpunardawid@gmail.com>
2024-12-16 20:14:27 +0100

    add terraform main file

--- main.tf
+++ main.tf
@@ -1,1 +1,32 @@
-Hello world
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-west-1"
+}
+
+# Create a EC2
+resource "aws_instance" "demo-instance" {
+  ami           = "ami-02141377eee7defb9"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "demo-ec2-instance"
+  }
+}
+
+resource "aws_instance" "demo2-instance" {
+  ami           = "ami-02141377eee7defb9"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "demo2-ec2-instance"
+  }
+}
